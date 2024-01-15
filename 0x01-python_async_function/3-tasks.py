4ed8cc6aec1f4d223cbc9951b2f97874aa031410
hillary-254 <hillarykim001@gmail.com>
2024-01-15 18:29:02 +0300

    3. Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Module documentation for 3-tasks.py
+"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Regular function that take an integer max_delay and return an asyncio.Task
+
+    :param max_delay: The maximum delay in seconds.
+    :type max_delay: int
+    :return: An asyncio.Task.
+    :rtype: asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
+
+if __name__ == "__main__":
+    import asyncio
+
+    async def test(max_delay: int) -> None:
+        task = task_wait_random(max_delay)
+        await task
+        print(task.__class__)
+
+    asyncio.run(test(5))

2af100484af3652cafa037256f331d6bd083a4a3
hillary-254 <hillarykim001@gmail.com>
2024-01-15 18:51:15 +0300

    fixing code

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,16 +12,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Async routine spawns wait_random n times with specified max_delay
-
-    :param n: The number of times to spawn wait_random.
-    :type n: int
-    :param max_delay: The maximum delay in seconds.
-    :type max_delay: int
-    :return: List of delays (float values) in ascending order.
-    :rtype: List[float]
     """
-    delays = []
-    for _ in range(n):
-        delays.append(await wait_random(max_delay))
-
-    return sorted(delays)
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -12,13 +12,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Async routine spawns task_wait_random n times with the specified max_delay
-
-    :param n: The number of times to spawn task_wait_random.
-    :type n: int
-    :param max_delay: The maximum delay in seconds.
-    :type max_delay: int
-    :return: List of delays (float values) in ascending order.
-    :rtype: List[float]
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
